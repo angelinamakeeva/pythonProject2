a4a50adf18a907ec7d9947b32d3bdf0311da1397
angelinamakeeva <125530626+angelinamakeeva@users.noreply.github.com>
2023-09-19 05:53:36 +0300

    working with changes from Alena and Nastya

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #Makeeva Angelina 85%
 #Kareva Alena 70%
+#Osokina Anastasya 80%
 
-
+import ru_local as ru
 from textblob import TextBlob
 import nltk
 import re
 nltk.download('punkt')
-text ="У нас росла липа. Липа стала стара. Липа стала суха. Липа упала. Пришли папа и Паша. У папы пила. У Паши топорик. Они распилили липу."
-glas = 'ауоыэяюёиеАУОЫЭЯЮЁИЕ'
-glasen = "AEIOUYaeiouy"
+text = input(ru.text)
+glas = ru.ru_vowels
+glasen = ru.en_vowels
 slogi = 0
 
 def sent_count(text):
@@ -54,46 +55,33 @@ else:
 
 
 if detect_language == 'en':
-  flash_en = 206.835 - (1.015*(words/sentences))-(84.6*(slogi/words))
+  flash_en = 206.835 - (1.015 * (words / sentences)) - (84.6 * (slogi / words))
 if detect_language == 'ru':
-  flash_ru = 206.835 - (1.3*(words/sentences))-(60.1*(slogi/words))
+  flash_ru = 206.835 - (1.3 * (words / sentences)) - (60.1 * (slogi / words))
 
 
-print('Предложений: ', sentences)
-print('Слов: ', words)
-print('Слогов: ', slogi)
-print('Средняя длина предложения в словах: ', total_len_by_words)
-print('Средняя длина слова в слогах: ', total_len_by_slogi)
+print(ru.sentence, sentences)
+print(ru.words, words)
+print(ru.syllables, slogi)
+print(ru.sr_word, total_len_by_words)
+print(ru.sr_syllables, total_len_by_slogi)
 if detect_language == 'en':
-  print('Индекс удобочитаемости Флеша: ', flash_en)
+  print(ru.flash, flash_en)
   if flash_en > 80.0:
-    print("Текст очень легко читается (для младших школьников).")
+    print(ru.easy)
   if flash_en > 50.0 and flash_en <=80.0:
-    print("Простой текст(для школьников).")
+    print(ru.simple)
   if flash_en > 25.0 and flash_en <= 50.0 :
-    print("Текст немного трудно читать (для студентов).")
+    print(ru.difficult)
   if flash_en <= 25.0 :
-    print("Текст трудно читается (для выпускников ВУЗов).")
+    print(ru.hard)
 else:
-  print('Индекс удобочитаемости Флеша: ', flash_ru)
+  print(ru.flash, flash_ru)
   if flash_ru > 80.0:
-    print("Текст очень легко читается (для младших школьников).")
+    print(ru.easy)
   if flash_ru > 50.0 and flash_ru <= 80.0:
-    print("Простой текст(для школьников).")
+    print(ru.simple)
   if flash_ru > 25.0 and flash_ru <= 50.0 :
-    print("Текст немного трудно читать (для студентов).")
+    print(ru.difficult)
   if flash_ru <= 25.0 :
-    print("Текст трудно читается (для выпускников ВУЗов).")
-
-
-pol = textblob.polarity
-if pol  == 0.0  or pol < 0.3:
-  print("Тональность текста: ",'нейтральный')
-if pol < 0.0 :
-  print("Тональность текста: ",'негативный')
-if pol > 0.3 :
-  print("Тональность текста: ",'позитивный')
-
-sub = textblob.subjectivity
-print('Объективность: ', round(100 - sub*100),'%', sep='')
-
+    print(ru.hard)
